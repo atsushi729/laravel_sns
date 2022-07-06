54d1115171c38cf8ffdc2ec4028ab9bce92236cd
atusi <atu729@icloud.com>
2022-07-06 09:25:57 +0900

    add base post function

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,7 +3,21 @@
 @section('content')
     <div class="flex justify-center" id="post">
         <div class="w-8/12 bg-white p-6 rounded-lg">
-            Post
+            <form action="{{ route('posts') }}" method="post">
+                @csrf
+                <div class="mb-4">
+                    <textarea name="body" id="body" cols="30" rows="4" class="bg-gray-100 @error('body') border-red-500 @enderror"
+                    placeholder="Post something"></textarea>
+                    @error('body')
+                        <div class="error_message">
+                            {{ $message }}
+                        </div>
+                    @enderror
+                </div>
+                <div>
+                    <button type="submit" class="text-white" id="button">Post</button>
+                </div>
+            </form>
         </div>
     </div>
 @endsection
