ee0b5baf57786cb17e831ebbcb055d690b45f8aa
atusi <atu729@icloud.com>
2022-07-07 23:55:16 +0900

    add display feature to show posted info

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,11 +3,12 @@
 @section('content')
     <div class="flex justify-center" id="post">
         <div class="w-8/12 bg-white p-6 rounded-lg">
-            <form action="{{ route('posts') }}" method="post">
+            <form action="{{ route('posts') }}" method="post" class="mb-4">
                 @csrf
                 <div class="mb-4">
                     <textarea name="body" id="body" cols="30" rows="4" class="bg-gray-100 @error('body') border-red-500 @enderror"
                     placeholder="Post something"></textarea>
+
                     @error('body')
                         <div class="error_message">
                             {{ $message }}
@@ -15,9 +16,20 @@
                     @enderror
                 </div>
                 <div>
-                    <button type="submit" class="text-white" id="button">Post</button>
+                    <button type="submit" class="submit" id="button">Post</button>
                 </div>
             </form>
+
+            @if ($posts->count())
+                @foreach($posts as $post)
+                    <div class="mb-4">
+                        <a href="" class="name">{{ $post->user->name }}</a><span class="date">{{ $post->created_at->diffForHumans() }}</span>
+                        <p class="body-text">{{ $post->body }}</p>
+                    </div>
+                @endforeach
+            @else
+                <p>There are no posts</p>
+            @endif
         </div>
     </div>
 @endsection
